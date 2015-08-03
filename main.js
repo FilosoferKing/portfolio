2d208e916be0b858ce778c4c5c40f74b3ee04fe3
filosoferking <filosoferking@yahoo.com>
2015-08-03 15:11:28 -0700

    started about me page

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+
+
 $(document).ready(function() {
     $('#arrow').click('on', function() {
-        $('.line_shift').css({"transform": "translateX(0px) rotateZ(360deg)",
+        $('.line_shift').css({"transform": "translateX(0px)",
                                "transition-duration": "1s",
                                 "transition-timing-function": "ease-out"});
         $('.landing_div:hover .right_nav_menu .nav_item p:nth-child(1)').css({
@@ -31,8 +33,16 @@ $(document).ready(function() {
             "transition-delay": "1.75s",
             "opacity": "1"
         });
+        $('#arrow').hide();
         console.log("click");
     });
 
+    $('body').mouseleave(function(){
+        $('.landing_div .right_nav_menu .nav_item p:nth-child(1), .landing_div .right_nav_menu .nav_item p:nth-child(2), .landing_div .right_nav_menu .nav_item p:nth-child(3), .landing_div .right_nav_menu .nav_item p:nth-child(4)').css({"opacity": "0", "transform": "translateX(250px)"});
+        $('.line_shift').css({"transform": "translateX(-100%)"});
+        $('#arrow').show();
+    });
 
 });
+
+
