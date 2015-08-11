54050f278238a7e7e484d96f5def67a46f195995
filosoferking <filosoferking@yahoo.com>
2015-08-11 09:39:36 -0700

    new navigation working

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    var menu_toggle = false;//toggle variable for navigation menu click handler
+
+
+    /***************************************
+     * NAME: Menu button click handler
+     * GLOBAL VARIABLES: menu_toggle
+     * PURPOSE: Click handler for the menu button that toggles the
+     * navigation menu on and off
+     */
+    $('.menu_button').on('click', function () {
+        if (menu_toggle == false) {
+            $('.nav_menu').css({"visibility": "visible"});
+            menu_toggle = true;
+        } else {
+            $('.nav_menu').css({"visibility": "hidden"});
+            menu_toggle = false;
+        }
+    });
 
 });
