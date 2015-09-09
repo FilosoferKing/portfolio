8095492c369900a260dc85045541e8bb8bf4b9b3
filosoferking <filosoferking@yahoo.com>
2015-09-08 20:11:52 -0700

    update

--- carousel.js
+++ carousel.js
@@ -37,8 +37,8 @@ $(document).ready(function(){
             clickEnabled = false;
             var imageButtonId = $(this).attr('id');
             console.log("Button clicked - Id: ", imageButtonId);
-            $('#' + currentImage).animate({'left': '-100%'}, 500);
-            $('.projectImagesContainer #' + imageButtonId).delay(500).animate({'left': '0'}, 500);
+            $('#image' + currentImage).animate({'left': '-100%'}, 500);
+            $('.projectImagesContainer #image' + imageButtonId).delay(500).animate({'left': '0'}, 500);
             currentImage = imageButtonId;
             setTimeout(function () {
                 clickEnabled = true;
@@ -46,14 +46,14 @@ $(document).ready(function(){
         }
     });
 
-    $('#' + currentImage).css({'left': '0'});
+    $('#image' + currentImage).css({'left': '0'});
 
 });
 function loadProjectImage(){
     for (var i = projectImageArray.length; i > -1; i--) {
         var projectImageDiv = $('<div>', {
             class: 'col-xs-12 imageDiv',
-            id: i
+            id: 'image' + i
         });
         $(projectImageDiv).css({'background': 'transparent url("img/' + projectImageArray[i] + '") no-repeat 50% 50%', 'background-size': 'cover'});
         $(projectImageDiv).appendTo('.projectImagesContainer');
@@ -66,14 +66,14 @@ function displayNextImage(direction){
     if(direction == -1 && currentImage == 0){
         stagedImage = 7;
         console.log('Current Image Left at 0: ', currentImage);
-        $('#' + stagedImage).delay(500).animate({'left': '0'}, 500);
-        $('#' + currentImage).animate({'left': '-100%'}, 500);
+        $('#image' + stagedImage).delay(500).animate({'left': '0'}, 500);
+        $('#image' + currentImage).animate({'left': '-100%'}, 500);
         currentImage = stagedImage;
         console.log('New current image left at 0: ', currentImage);
     } else if(direction == -1) {
         console.log('Current Image Left: ', currentImage);
-        $('#' + stagedImage).delay(500).animate({'left': '0'}, 500);
-        $('#' + currentImage).animate({'left': '-100%'}, 500);
+        $('#image' + stagedImage).delay(500).animate({'left': '0'}, 500);
+        $('#image' + currentImage).animate({'left': '-100%'}, 500);
         currentImage = stagedImage;
         console.log('New current image left: ', currentImage);
     }
@@ -82,14 +82,14 @@ function displayNextImage(direction){
 
         console.log('Current Image Right at 7: ', currentImage);
         stagedImage = 0;
-        $('#' + stagedImage).delay(500).animate({'left': '0'}, 500);
-        $('#' + currentImage).animate({'left': '-100%'}, 500);
+        $('#image' + stagedImage).delay(500).animate({'left': '0'}, 500);
+        $('#image' + currentImage).animate({'left': '-100%'}, 500);
         currentImage = stagedImage;
         console.log('New current image at 7: ', currentImage);
     } else if(direction == 1) {
         console.log('Current Image Right: ', currentImage);
-        $('#' + stagedImage).delay(500).animate({'left': '0'}, 500);
-        $('#' + currentImage).animate({'left': '-100%'}, 500);
+        $('#image' + stagedImage).delay(500).animate({'left': '0'}, 500);
+        $('#image' + currentImage).animate({'left': '-100%'}, 500);
         currentImage = stagedImage;
         console.log('New current image right: ', currentImage);
     }
