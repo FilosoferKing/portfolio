d64f9c20c177de429088bbe1f558374d4267dd2d
filosoferking <filosoferking@yahoo.com>
2015-09-13 15:10:32 -0700

    need to fix nav button color

--- main.js
+++ main.js
@@ -24,8 +24,16 @@ $(document).ready(function () {
     //$('.nav_container').css({"background-color": "rgba(0, 0, 0, .1)", "top": "0", "height": "8vh"});
     nav_p_slide_down();
 
-    $('.menu_click').on('click', function(){
+    /*Handler for correcting toggle flag when a nave menu option is clicked.*/
+    $('.menu_click').on('click', function () {
         menu_toggle = false;
+    });
+
+    /*Handler for hovering over a specific project*/
+    $('.imageDiv').on('mouseover', function () {
+        var projectId = $(this).attr('id');
+        console.log('Project id: ', projectId);
+        changeProjectDescription(projectId);
     });
 
 
@@ -43,100 +51,146 @@ $(document).ready(function () {
             //$('.nav_container').css({"background-color": "rgba(0, 0, 0, .25)", "top": "0", "height": "8vh"});
             menu_toggle = true;
         } else {
-            $('.nav_menu').css({"display": "none",  "transition-duration": "0s"});
+            $('.nav_menu').css({"display": "none", "transition-duration": "0s"});
             //$('.nav_container').css({"background-color": "rgba(0, 0, 0, 0)", "top": "0", "height": "0"});
             menu_toggle = false;
         }
     });
 });
-    /***************************************
-     * NAME: Scroll handler
-     * PARAMS: none
-     * GLOBAL VARIABLES: none
-     * LOCAL VARIABLES: bottomPosition, sectionArray, trigger_position, highlighted_section
-     * PURPOSE: activates nav menu section based on what page section is being displayed
-     * FUNCTIONS USED: none
-     */
-    $(document).scroll(function () {
-        var bottomPosition = $(window).scrollTop() + $(window).height();
-        //console.log('Bottom position: ', bottomPosition);
+/***************************************
+ * NAME: Scroll handler
+ * PARAMS: none
+ * GLOBAL VARIABLES: none
+ * LOCAL VARIABLES: bottomPosition, sectionArray, trigger_position, highlighted_section
+ * PURPOSE: activates nav menu section based on what page section is being displayed
+ * FUNCTIONS USED: none
+ */
+$(document).scroll(function () {
+    var bottomPosition = $(window).scrollTop() + $(window).height();
+    //console.log('Bottom position: ', bottomPosition);
 
-        var sectionArray = [];
+    var sectionArray = [];
 
-        $('.trigger_point').each(function () {
-            var trigger_position = $(this).offset().top;
-            //console.log('Target position: ', trigger_position);
-            if (trigger_position <= bottomPosition) {
-                sectionArray[sectionArray.length] = this;
-                //console.log('SectionArray ', sectionArray);
-            }
-        });
-
-        $('.about, .projects, .skills, .contact').removeClass('js_hover');
-
-        if (sectionArray.length) {
-            var highlighted_section = $(sectionArray[sectionArray.length - 1]).attr('data-indicator');
-            //console.log('Data-indicator: ', highlighted_section);
-            $(highlighted_section).addClass('js_hover');
-
-            $('.title').hide();
-            $('.menu_button').fadeIn(1000);
-            $('.nav_menu').css({"display": "none"});
-
-        } else {
-            $('.nav_menu').css({"display": "block"});
-            $('.title').fadeIn(1000);
-            $('.menu_button').fadeOut(1000);
+    $('.trigger_point').each(function () {
+        var trigger_position = $(this).offset().top;
+        //console.log('Target position: ', trigger_position);
+        if (trigger_position <= bottomPosition) {
+            sectionArray[sectionArray.length] = this;
+            //console.log('SectionArray ', sectionArray);
         }
     });
 
-    /***************************************
-     * NAME: fade_title
-     * PARAMS: none
-     * GLOBAL VARIABLES: none
-     * LOCAL VARIABLES: none
-     * PURPOSE: Fades in and out name title and loads nav background br
-     * FUNCTIONS USED: nav_p_slide_down()
-     */
-    function fade_title() {
-        setTimeout(function () {
-            $('.word1 .char1').css({'transform': 'scaleY(1.5)scaleX(1.5)'});
-            $('.word1 .char2').css({'transform': 'scaleY(1) scaleX(1)'});
-            $('.word1 .char3').css({'transform': 'scaleY(1) scaleX(1)'});
-            $('.word1 .char4').css({'transform': 'scaleY(1) scaleX(1)'});
-            $('.word1 .char5').css({'transform': 'scaleY(1) scaleX(1)'});
-            $('.word1 .char6').css({'transform': 'scaleY(1) scaleX(1)'});
-        }, 500);
+    $('.about, .projects, .skills, .contact').removeClass('js_hover');
 
-        setTimeout(function () {
-            $('.word2 .char1').css({'transform': 'scaleY(1) scaleX(1)'});
-            $('.word2 .char2').css({'transform': 'scaleY(1) scaleX(1)'});
-            $('.word2 .char3').css({'transform': 'scaleY(1) scaleX(1)'});
-            $('.word2 .char4').css({'transform': 'scaleY(1) scaleX(1)'});
-            $('.word2 .char5').css({'transform': 'scaleY(1.5)'});
-        }, 1100);
-    };
+    if (sectionArray.length) {
+        var highlighted_section = $(sectionArray[sectionArray.length - 1]).attr('data-indicator');
+        //console.log('Data-indicator: ', highlighted_section);
+        $(highlighted_section).addClass('js_hover');
 
-    /***************************************
-     * NAME: nav_p_slide_down
-     * PARAMS: none
-     * GLOBAL VARIABLES: none
-     * LOCAL VARIABLES: none
-     * PURPOSE: slides in nav p elements from top of page into menu bar
-     * FUNCTIONS USED:
-     */
-    function nav_p_slide_down() {
-        $('.about_click').animate({'top': '50%'}, 1000);
-        setTimeout(function () {
-            $('.skills_click').animate({'top': '50%'}, 1000);
-        }, 250);
-        setTimeout(function () {
-            $('.projects_click').animate({'top': '50%'}, 1000);
-        }, 500);
-        setTimeout(function () {
-            $('.contact_click').animate({'top': '50%'}, 1000);
-        }, 750);
-    };
+        $('.title').hide();
+        $('.menu_button').fadeIn(1000);
+        $('.nav_menu').css({"display": "none"});
+
+    } else {
+        $('.nav_menu').css({"display": "block"});
+        $('.title').fadeIn(1000);
+        $('.menu_button').fadeOut(1000);
+    }
+});
+
+/***************************************
+ * NAME: fade_title
+ * PARAMS: none
+ * GLOBAL VARIABLES: none
+ * LOCAL VARIABLES: none
+ * PURPOSE: Fades in and out name title and loads nav background br
+ * FUNCTIONS USED: nav_p_slide_down()
+ */
+function fade_title() {
+    setTimeout(function () {
+        $('.word1 .char1').css({'transform': 'scaleY(1.5)scaleX(1.5)'});
+        $('.word1 .char2').css({'transform': 'scaleY(1) scaleX(1)'});
+        $('.word1 .char3').css({'transform': 'scaleY(1) scaleX(1)'});
+        $('.word1 .char4').css({'transform': 'scaleY(1) scaleX(1)'});
+        $('.word1 .char5').css({'transform': 'scaleY(1) scaleX(1)'});
+        $('.word1 .char6').css({'transform': 'scaleY(1) scaleX(1)'});
+    }, 500);
+
+    setTimeout(function () {
+        $('.word2 .char1').css({'transform': 'scaleY(1) scaleX(1)'});
+        $('.word2 .char2').css({'transform': 'scaleY(1) scaleX(1)'});
+        $('.word2 .char3').css({'transform': 'scaleY(1) scaleX(1)'});
+        $('.word2 .char4').css({'transform': 'scaleY(1) scaleX(1)'});
+        $('.word2 .char5').css({'transform': 'scaleY(1.5)'});
+    }, 1100);
+};
+
+/***************************************
+ * NAME: nav_p_slide_down
+ * PARAMS: none
+ * GLOBAL VARIABLES: none
+ * LOCAL VARIABLES: none
+ * PURPOSE: slides in nav p elements from top of page into menu bar
+ * FUNCTIONS USED:
+ */
+function nav_p_slide_down() {
+    $('.about_click').animate({'top': '50%'}, 1000);
+    setTimeout(function () {
+        $('.skills_click').animate({'top': '50%'}, 1000);
+    }, 250);
+    setTimeout(function () {
+        $('.projects_click').animate({'top': '50%'}, 1000);
+    }, 500);
+    setTimeout(function () {
+        $('.contact_click').animate({'top': '50%'}, 1000);
+    }, 750);
+};
+
+/***************************************
+ * NAME: changeProjectDescripton
+ * PARAMS: id
+ * GLOBAL VARIABLES: none
+ * LOCAL VARIABLES: replaceText
+ * PURPOSE: this replaces the content text of the ::after pseudo div when a specific project is hovered
+ * FUNCTIONS USED:
+ */
+function changeProjectDescription(id) {
+    //var built = $('<p>', {
+    //    text: $('#' + id).attr('built')
+    //});
+    //var language = $('<p>', {
+    //    text: $('#' + id).attr('lang')
+    //});
+    //var seeDescription = $('<p>', {
+    //    text: $('#' + id).attr('click')
+    //});
+    var replaceText = $('.imageDiv:hover::before').css({"content": "attr('built')"});
+    switch (id) {
+        case memorymatch:
+            replaceText;
+            break;
+        case calculator:
+            replaceText;
+            break;
+        case sgt:
+            replaceText;
+            break;
+        case twailerz:
+            replaceText;
+            break;
+        case tictactoe:
+            replaceText;
+            break;
+        case taskit:
+            replaceText;
+            break;
+        case final:
+            replaceText;
+            break;
+    }
+}
+
+
 
 
 
