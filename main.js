ad9172d3db55fc54988066bc86c8d62e95e9aa87
filosoferking <filosoferking@yahoo.com>
2015-09-16 23:45:43 -0700

    code clean up

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
     var menu_toggle = false;//toggle variable for navigation menu click handler
 
-    $('.nav_container').hide();
-    $('.menu_button').hide();
-    $('.title h2').hide();
-    //$('.title h1').hide();
-    //$('.title h2').hide();
+    //$('.nav_container').hide();
+    //$('.menu_button').hide();
+
     setTimeout(function () {
         $('.title h2').fadeIn(1000);
     }, 1750);
@@ -19,10 +17,10 @@ $(document).ready(function () {
         $('.left_nav').css({'transform': 'scaleX(1)'});
         $('.right_nav').css({'transform': 'scaleX(1)'});
     }, 3250);
-    fade_title();
+
     //$('.nav_container').fadeIn();
     //$('.nav_container').css({"background-color": "rgba(0, 0, 0, .1)", "top": "0", "height": "8vh"});
-    nav_p_slide_down();
+
 
     /*Handler for correcting toggle flag when a nave menu option is clicked.*/
     $('.menu_click').on('click', function () {
@@ -98,54 +96,6 @@ $(document).scroll(function () {
         $('.menu_button').fadeOut(1000);
     }
 });
-
-/***************************************
- * NAME: fade_title
- * PARAMS: none
- * GLOBAL VARIABLES: none
- * LOCAL VARIABLES: none
- * PURPOSE: Fades in and out name title and loads nav background br
- * FUNCTIONS USED: nav_p_slide_down()
- */
-function fade_title() {
-    setTimeout(function () {
-        $('.word1 .char1').css({'transform': 'scaleY(1.5)scaleX(1.5)'});
-        $('.word1 .char2').css({'transform': 'scaleY(1) scaleX(1)'});
-        $('.word1 .char3').css({'transform': 'scaleY(1) scaleX(1)'});
-        $('.word1 .char4').css({'transform': 'scaleY(1) scaleX(1)'});
-        $('.word1 .char5').css({'transform': 'scaleY(1) scaleX(1)'});
-        $('.word1 .char6').css({'transform': 'scaleY(1) scaleX(1)'});
-    }, 500);
-
-    setTimeout(function () {
-        $('.word2 .char1').css({'transform': 'scaleY(1) scaleX(1)'});
-        $('.word2 .char2').css({'transform': 'scaleY(1) scaleX(1)'});
-        $('.word2 .char3').css({'transform': 'scaleY(1) scaleX(1)'});
-        $('.word2 .char4').css({'transform': 'scaleY(1) scaleX(1)'});
-        $('.word2 .char5').css({'transform': 'scaleY(1.5)'});
-    }, 1100);
-};
-
-/***************************************
- * NAME: nav_p_slide_down
- * PARAMS: none
- * GLOBAL VARIABLES: none
- * LOCAL VARIABLES: none
- * PURPOSE: slides in nav p elements from top of page into menu bar
- * FUNCTIONS USED:
- */
-function nav_p_slide_down() {
-    $('.about_click').animate({'top': '50%'}, 1000);
-    setTimeout(function () {
-        $('.skills_click').animate({'top': '50%'}, 1000);
-    }, 250);
-    setTimeout(function () {
-        $('.projects_click').animate({'top': '50%'}, 1000);
-    }, 500);
-    setTimeout(function () {
-        $('.contact_click').animate({'top': '50%'}, 1000);
-    }, 750);
-};
 
 /***************************************
  * NAME: changeProjectDescripton
